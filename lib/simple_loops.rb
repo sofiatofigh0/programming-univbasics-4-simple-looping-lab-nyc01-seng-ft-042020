b1ecf2cd0a9de4e1afbe62be591ee3fcc972650c
sofia tofigh <sst2140@columbia.edu>
2020-04-02 06:00:47 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,33 @@
+def loop_message_five_times(string)
+  puts "#{string}"
+  puts "#{string}"
+  puts "#{string}"
+  puts "#{string}"
+  puts "#{string}"
+end
+  
+  
+def loop_message_n_times(string,integer)
+  
+  integer.times do
+    puts string
+end
+end
+
+def output_array(array)
+  
+  array.each do |x|
+    puts x
+end
+end
+
+def return_string_array(array)
+  
+  stringArray = []
+   array.each do |i|
+    stringArray = stringArray + i.to_s
+  end
+ return stringArray
+end
+
 # Write your methods here
